3a1faceae613a8fecb52589e02b44c433f837939
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 12:22:52 +0530

    Event Handler commentActionHandler: An asynchronous function that handles the submission of a new comment: It checks if the user is authenticated (user exists). Calls createCommentAction(postId, formData) to send the comment data to the server. Handles errors that may occur during the API request.

--- components/CommentInput.tsx
+++ components/CommentInput.tsx
@@ -8,7 +8,14 @@ import { createCommentAction } from '@/lib/serveractions'
 
 const CommentInput = ({ postId }: { postId: string }) => {
     const { user } = useUser();
-    
+    const commentActionHandler = async (formData:FormData) => {
+        try {
+            if(!user) throw new Error('User not authenticated');
+            await createCommentAction(postId, formData);
+        } catch (error) {
+            throw new Error('An error occured');
+        }
+    }
     
 }
 
