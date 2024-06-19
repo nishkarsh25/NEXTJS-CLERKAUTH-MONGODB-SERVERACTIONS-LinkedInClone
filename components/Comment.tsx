957cea37f0829ce2ae9ce8042680425c7c4f23a5
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 12:25:23 +0530

    Component Definition Comment: This functional component takes a single prop comment of type ICommentDocument. It renders a single comment with details such as the commenter's profile photo, name, username, comment text, and the comment's creation time.

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -3,5 +3,7 @@ import React from 'react'
 import ProfilePhoto from './shared/ProfilePhoto'
 import ReactTimeago from 'react-timeago'
 
-
+const Comment = ({ comment }: { comment: ICommentDocument }) => {
+    
+}
 
