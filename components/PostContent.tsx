18e000ceb9575f76482e313ac1faedbf44eb73a9
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 11:52:14 +0530

    Component Definition Props post: An object representing the post, conforming to the IPostDocument interface. This prop contains all the necessary data to render the content of the post.

--- components/PostContent.tsx
+++ components/PostContent.tsx
@@ -2,4 +2,7 @@ import { IPostDocument } from '@/models/post.model'
 import Image from 'next/image'
 import React from 'react'
 
+const PostContent = ({ post }: { post: IPostDocument }) => {
+  
+}
 
