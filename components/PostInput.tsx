7f4b6b16e52de73f6a5171b0aa20a2e7ffcb5853
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 11:04:08 +0530

    Import Statements React and useState:
    import React, { useState } from 'react': Imports React and the useState hook to manage component state.
    ProfilePhoto:
    
    import ProfilePhoto from './shared/ProfilePhoto': Imports the ProfilePhoto component to display the user's profile picture.
    Input:
    
    import { Input } from './ui/input': Imports a custom Input component for the input field.
    PostDialog:
    
    import { PostDialog } from './PostDialog': Imports the PostDialog component which will be used to create a new post when the input field is clicked.

---
+++ components/PostInput.tsx
@@ -0,0 +1,8 @@
+'use client';
+import React, { useState } from 'react'
+import ProfilePhoto from './shared/ProfilePhoto'
+import { Input } from './ui/input'
+import { PostDialog } from './PostDialog'
+
+
+
