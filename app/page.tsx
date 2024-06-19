2c2bafe80db1803e627509dd78dc925d66443175
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 12:26:52 +0530

    Component Definition: Home Function: Home is an asynchronous function (async function Home()) that fetches the current user's information using currentUser() from the Clerk authentication library.

--- app/page.tsx
+++ app/page.tsx
@@ -4,4 +4,8 @@ import Sidebar from "@/components/Sidebar";
 import { currentUser } from "@clerk/nextjs/server";
 
  
-
+export default async function Home() {
+  const user = await currentUser();
+   
+  
+}
