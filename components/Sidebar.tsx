b9ff2805a2d9beca1442f060c33f0a353df8bd8f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 10:54:28 +0530

    Sidebar Component Sidebar Component Definition: const Sidebar = async ({ user }: { user: any }) => { ... }: Defines an asynchronous functional component Sidebar that accepts a user prop. The prop user is expected to contain user information.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,5 +3,9 @@ import React from 'react'
 import ProfilePhoto from './shared/ProfilePhoto'
 import { getAllPosts } from '@/lib/serveractions'
 
+const Sidebar = async ({ user }: { user: any }) => {
 
+    
+    
+}
 
