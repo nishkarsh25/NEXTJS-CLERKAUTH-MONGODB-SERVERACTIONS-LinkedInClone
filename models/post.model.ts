e1b42aec6c2603c20c20add197f184734425fe90
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 10:19:16 +0530

    Interface Definitions export interface IPost {
    Defines an IPost interface, outlining the structure of a post object.
    description: string;
    
    A string representing the description of the post.
    user: IUser;
    
    An object representing the user who created the post, adhering to the IUser interface.
    imageUrl?: string;
    
    An optional string representing the URL of the image associated with the post.
    likes?: string[];
    
    An optional array of strings representing the IDs of users who liked the post.
    comments?: IComment[];
    
    An optional array of comment objects adhering to the IComment interface.
    export interface IPostDocument extends IPost, Document {
    
    Defines an IPostDocument interface that extends both IPost and Document.
    This interface represents a post document in MongoDB with additional Mongoose document properties.
    createdAt: Date;
    
    A date representing when the post was created.
    updatedAt: Date;
    
    A date representing when the post was last updated.

--- models/post.model.ts
+++ models/post.model.ts
@@ -1,5 +1,15 @@
 import mongoose, { Document, Model } from "mongoose";
 import { IUser } from "./user.model";
 import { IComment } from "./comment.model";
-
-
+export interface IPost{
+    description:string,
+    user:IUser,
+    imageUrl?:string,
+    likes?:string[],
+    comments?:IComment[]
+}
+export interface IPostDocument extends IPost, Document{
+    createdAt:Date,
+    updatedAt:Date
+}
+const postSchema = new mongoose.Schema<IPostDocument>();
