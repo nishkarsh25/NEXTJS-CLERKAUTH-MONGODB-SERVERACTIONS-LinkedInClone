eb77a8ead2479002c940b5cf1c3cd12c2c5357ec
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 06:16:28 +0530

    Schema Definition const commentSchema = new mongoose.Schema<ICommentDocument>({
    Defines a Mongoose schema named commentSchema for ICommentDocument.
    textMessage: { type: String, required: true },
    
    textMessage: A required string field.
    user: {
    
    user: An embedded document defining user details within the comment.
    userId: { type: String, required: true },
    
    userId: A required string field representing the user ID.
    profilePhoto: { type: String, required: true },
    
    profilePhoto: A required string field representing the URL of the user's profile photo.
    firstName: { type: String, required: true },
    
    firstName: A required string field representing the user's first name.
    lastName: { type: String, required: true },
    
    lastName: A required string field representing the user's last name.

--- models/comment.model.ts
+++ models/comment.model.ts
@@ -10,4 +10,27 @@ export interface ICommentDocument extends IComment, Document{
     updatedAt:Date
 }
 
-const commentSchema = new mongoose.Schema<ICommentDocument>();
+const commentSchema = new mongoose.Schema<ICommentDocument>({
+    textMessage:{
+        type:String,
+        required:true
+    },
+    user:{
+        userId:{
+            type:String,
+            required:true
+        },
+        profilePhoto:{
+            type:String,
+            required:true
+        },
+        firstName:{
+            type:String,
+            required:true
+        },
+        lastName:{
+            type:String,
+            required:true
+        }
+    }
+});
