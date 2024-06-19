ca5470242d380b027b5e9203d2504b1c4264c3de
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 10:50:51 +0530

    Database Connection Connects to the database: await connectDB(): This line ensures that the function establishes a connection to the MongoDB database before performing any database operations. Fetching the Post Finds the post by its ID:
    const post = await Post.findById({_id: params.postId}): This line queries the database to find the post with the specified postId.
    Handles case where post is not found:
    
    if (!post) return NextResponse.json({error: "Post not found."}): If the post is not found in the database, the function returns a JSON response indicating that the post was not found.
    Removing Like
    Updates the post to remove the user's like:
    await post.updateOne({$pull: {likes: userId}}): This line updates the post document to remove the userId from the likes array using MongoDB's $pull operator. This effectively removes the user's like from the post.
    Returning Response
    Returns success message:
    return NextResponse.json({message: "Post disliked successfully."}): After successfully removing the like, the function returns a JSON response with a success message indicating that the post was disliked successfully.

--- app/api/posts/[postId]/dislike/route.ts
+++ app/api/posts/[postId]/dislike/route.ts
@@ -4,5 +4,14 @@ import { NextRequest, NextResponse } from "next/server";
 
 // post likes
 export const POST = async (req:NextRequest, {params}:{params:{postId:string}}) => {
-    
+    try {
+        await connectDB();
+        const userId = await req.json();
+        const post = await Post.findById({_id:params.postId});
+        if(!post) return NextResponse.json({error:'Post not found.'});
+        await post.updateOne({$pull:{likes:userId}});
+        return NextResponse.json({message:"Post disliked successfully."});
+    } catch (error:any) {
+        
+    }
 }
