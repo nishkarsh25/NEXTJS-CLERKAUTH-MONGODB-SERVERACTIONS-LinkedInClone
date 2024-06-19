a641563da6cda9c5a5808744ceca5c491f20f368
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 11:41:02 +0530

    Component Definition State and References inputRef: A reference to the file input element. selectedFile: A state variable to store the selected image file as a Data URL. inputText: A state variable to store the text input.

--- components/PostDialog.tsx
+++ components/PostDialog.tsx
@@ -15,3 +15,15 @@ import Image from "next/image"
 import { createPostAction } from "@/lib/serveractions"
 import { toast } from "sonner"
 
+export function PostDialog({ setOpen, open, src }: { setOpen: any, open: boolean, src: string }) {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [selectedFile, setSelectedFile] = useState<string>("");
+    const [inputText, setInputText] = useState<string>("");
+
+   
+
+    
+    
+
+    
+}
