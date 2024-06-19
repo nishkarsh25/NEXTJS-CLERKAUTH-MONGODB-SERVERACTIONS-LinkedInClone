23d5c1f270b7974a1cc5060659ffa84322fc1f5b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 10:56:32 +0530

    Type Definition (Optional) Interface Definition: interface NAVITEMS { ... }: Defines an interface NAVITEMS that specifies the structure of each navigation item (src, icon, text). This is commented out (method-1 to create type), indicating an alternative approach to type definition.

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,7 +1,19 @@
 import { Bell, BriefcaseBusiness, Home, MessageCircleMore, Users } from 'lucide-react'
 import Link from 'next/link';
 import React from 'react';
+// method-1 to create type
+// type NAVITEMS = {
+//     src:string,
+//     icon:JSX.Element,
+//     text:string
+// }
 
+// m-2 to create type
+interface NAVITEMS {
+    src:string,
+    icon:JSX.Element,
+    text:string
+}
 
 
 
