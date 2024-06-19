2ccfa3af8e2e3aaab313d3a10dfa62b61c0cb51f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 12:22:09 +0530

    Imports React: The fundamental library for building user interfaces in React. ProfilePhoto: A custom component imported from ./shared/ProfilePhoto responsible for displaying user profile photos. useUser: A custom hook from @clerk/nextjs used to get information about the current authenticated user. Input: A custom UI component imported from ./ui/input used for text input fields. Button: A custom UI component imported from ./ui/button used for clickable buttons. createCommentAction: A function imported from @/lib/serveractions responsible for making API requests to create a new comment associated with a post.

---
+++ components/CommentInput.tsx
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+import ProfilePhoto from './shared/ProfilePhoto'
+import { useUser } from '@clerk/nextjs'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
+import { createCommentAction } from '@/lib/serveractions'
+
+
+
