f8aca10a5dac686351853758203f955a81cc10d1
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 12:18:42 +0530

    Event Handlers likeOrDislikeHandler: This asynchronous function handles the logic for liking or disliking a post: It toggles the liked state. Updates the likes state based on whether the user is liking or disliking the post. Makes a POST request to /api/posts/${post._id}/${liked ? '/dislike' : '/like'} to update the like status on the server. Handles errors and reverts the state if there's a failure. Fetches the updated likes count from the server after the operation and updates the likes state.

--- components/SocialOptions.tsx
+++ components/SocialOptions.tsx
@@ -12,7 +12,38 @@ const SocialOptions = ({ post }: { post: IPostDocument }) => {
     const [likes, setLikes] = useState(post.likes);
     const [commentOpen, setCommentOpen] = useState(false);
 
-    
+    const likeOrDislikeHandler = async () => {
+        if (!user) throw new Error(' User not athenticated');
+        const tempLiked = liked;
+        const tempLikes = likes;
+        const dislike = likes?.filter((userId) => userId !== user.id);
+        const like = [...(likes ?? []), user.id];
+        const newLike = liked ? dislike : like;
+
+        setLiked(!liked);
+        setLikes(newLike);
+
+        const res = await fetch(`/api/posts/${post._id}/${liked ? '/dislike' : '/like'}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify(user.id),
+        });
+        if (!res.ok) {
+            setLiked(tempLiked);
+            throw new Error('Failed to like or dislike')
+        }
+
+        const fetchAllLikes = await fetch(`/api/posts/${post._id}/like`);
+        if (!fetchAllLikes.ok) {
+            setLikes(tempLikes);
+            throw new Error('Failed to fetch like');
+        }
+
+        const likeData = await fetchAllLikes.json();
+        setLikes(likeData);
+    }
     
 }
 
