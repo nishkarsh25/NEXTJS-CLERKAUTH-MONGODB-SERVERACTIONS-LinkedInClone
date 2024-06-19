4b7d1ee3f5a3c6e0de55f3106f5324915ba4c60d
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 11:46:48 +0530

    Import Statements React: The base library for building React components. ProfilePhoto: A component that displays the user's profile photo. useUser: A hook from @clerk/nextjs for accessing the current logged-in user. Button: A UI button component. Trash2: An icon from lucide-react for the delete button. Badge: A UI component to display badges. IPostDocument: An interface representing the structure of a post document, imported from the post model. PostContent: A component to display the content of the post. SocialOptions: A component for displaying social interaction options like likes and comments. ReactTimeago: A library for displaying time in a relative format (e.g., "3 hours ago"). deletePostAction

---
+++ components/Post.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react'
+import ProfilePhoto from './shared/ProfilePhoto'
+import { useUser } from '@clerk/nextjs'
+import { Button } from './ui/button'
+import { Trash2 } from 'lucide-react'
+import { Badge } from './ui/badge'
+import { IPostDocument } from '@/models/post.model'
+import PostContent from './PostContent'
+import SocialOptions from './SocialOptions'
+import ReactTimeago from "react-timeago";
+import { deletePostAction } from '@/lib/serveractions'
+
+
