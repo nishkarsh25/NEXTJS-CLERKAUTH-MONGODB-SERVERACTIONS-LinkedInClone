bd5e12cf98428afec4085208efb325bddbf899b1
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 10:38:57 +0530

    JSX Structure <div> Wrapper:
    The component is wrapped in a div element to provide a container for styling or additional structure.
    <Avatar className='cursor-pointer'>:
    
    The Avatar component is used to create a container for the profile photo.
    The className='cursor-pointer' applies a CSS class to change the cursor to a pointer when hovering over the avatar, indicating it is clickable.
    <AvatarImage src={src} alt="banner" />:
    
    The AvatarImage component is used to display the profile photo.
    src={src} sets the source of the image to the URL passed in the src prop.
    alt="banner" provides alternative text for the image, which is useful for accessibility and in case the image cannot be loaded.

--- components/shared/ProfilePhoto.tsx
+++ components/shared/ProfilePhoto.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 import { Avatar, AvatarImage } from '../ui/avatar'
 
 const ProfilePhoto = ({ src }: { src: string }) => {
-    
-    
+    return (
+        <div>
+            <Avatar className='cursor-pointer'>
+                <AvatarImage src={src} alt="banner" />
+            </Avatar>
+        </div>
+    )
 }
 
