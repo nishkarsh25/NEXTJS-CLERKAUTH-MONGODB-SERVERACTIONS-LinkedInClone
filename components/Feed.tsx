2403b9b46be311a0c7522fd7c4c710bae905a638
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 11:00:46 +0530

    Feed Component Feed Component Definition: const Feed = async ({ user }: { user: any }) => { ... }: Defines an asynchronous functional component Feed that accepts a prop user. The user prop is expected to contain user information.

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -3,5 +3,9 @@ import PostInput from './PostInput'
 import Posts from './Posts'
 import { getAllPosts } from '@/lib/serveractions';
 
-
+const Feed = async ({user}:{user:any}) => {
+   
+    
+  
+}
 
