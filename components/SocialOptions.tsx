b31d04f37cb9c0dcee964953f3326e18eae99830
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 12:17:56 +0530

    Import Statements React: The fundamental library for building user interfaces in React. useState: A React hook used to manage state within functional components. Button: A custom UI component imported from ./ui/button. Icons from Lucide-react: Icons imported from the Lucide React library for visual representation (ThumbsUp, MessageCircleMore, Repeat, Send). IPostDocument: An interface representing the structure of a post document, imported from @/models/post.model. useUser: A custom hook from @clerk/nextjs used to get information about the current authenticated user. CommentInput: A component imported from ./CommentInput responsible for rendering the input field to write a comment. Comments: A component imported from ./Comments responsible for rendering the list of comments associated with a post.

---
+++ components/SocialOptions.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+import { Button } from './ui/button'
+import { MessageCircleMore, Repeat, Send, ThumbsUp } from 'lucide-react'
+import { IPostDocument } from '@/models/post.model';
+import { useUser } from '@clerk/nextjs';
+import CommentInput from './CommentInput';
+import Comments from './Comments';
+
+
