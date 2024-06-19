456db8b2cf4b84579c9d7ea304efc746ba705df6
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 11:01:04 +0530

    JSX Rendering JSX Structure: <div className='flex-1'>: Defines a flex container that takes up all available space (flex-1).
    Inside the flex container:
    
    PostInput Component: Renders the PostInput component, passing userData as a prop. This component likely allows the user to input new posts.
    Posts Component: Renders the Posts component, passing posts as a prop. This component displays a list of posts fetched from the server.

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -7,6 +7,11 @@ const Feed = async ({user}:{user:any}) => {
     const userData = JSON.parse(JSON.stringify(user));
     const posts = await getAllPosts();
     
-  
+  return (
+    <div className='flex-1'>
+        <PostInput user={userData}/>
+        <Posts posts = {posts!}/>
+    </div>
+  )
 }
 
