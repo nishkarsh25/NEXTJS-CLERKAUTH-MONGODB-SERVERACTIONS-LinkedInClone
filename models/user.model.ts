e80214873d9f341ab4f5f09ada3c91375dcb0219
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 10:22:17 +0530

    Interface Definitions Defines the IUser interface:
    IUser: An interface that outlines the structure of a user object. It includes the following properties:
    firstName: A string representing the user's first name.
    lastName: A string representing the user's last name.
    userId: A string representing a unique identifier for the user.
    profilePhoto (optional): A string representing the URL of the user's profile photo.
    bio (optional): A string representing the user's biography.

--- models/user.model.ts
+++ models/user.model.ts
@@ -1,3 +1,9 @@
 import mongoose, { Document, Model } from "mongoose";
-
+export interface IUser{
+    firstName:string,
+    lastName:string,
+    userId:string,
+    profilePhoto?:string,
+    bio?:string
+}
 
