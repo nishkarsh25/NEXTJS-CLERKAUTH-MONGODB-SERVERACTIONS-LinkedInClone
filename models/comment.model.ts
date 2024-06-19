eaa5db99c7ae45f6a25f6df37721d215717d7511
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 06:15:56 +0530

    Interface Definitions export interface IComment {
    Defines an IComment interface, which outlines the structure of a comment object.
    textMessage: string;
    
    textMessage: A string representing the comment's text message.
    user: IUser;
    
    user: An object representing the user who made the comment, adhering to the IUser interface.
    export interface ICommentDocument extends IComment, Document {
    
    Defines an ICommentDocument interface, which extends both IComment and Document.
    This interface represents a comment document in MongoDB with additional Mongoose document properties.
    createdAt: Date;
    
    createdAt: A date representing when the comment was created.
    updatedAt: Date;
    
    updatedAt: A date representing when the comment was last updated.

--- models/comment.model.ts
+++ models/comment.model.ts
@@ -1,6 +1,13 @@
 import mongoose, { Model } from "mongoose";
 import { IUser } from "./user.model";
 import { Document } from "mongoose";
-
+export interface IComment{
+    textMessage:string,
+    user:IUser
+}
+export interface ICommentDocument extends IComment, Document{
+    createdAt:Date,
+    updatedAt:Date
+}
 
 
