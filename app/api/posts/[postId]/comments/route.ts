d76f5abed4d5125d00922863ac7cfb93bd5bc6e1
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 10:40:37 +0530

    GET Function Defines the GET function to handle GET requests: This function is an asynchronous function that handles fetching comments for a specific post. It's defined using the export keyword so it can be used as an API route.

--- app/api/posts/[postId]/comments/route.ts
+++ app/api/posts/[postId]/comments/route.ts
@@ -2,3 +2,7 @@ import connectDB from "@/lib/db";
 import { Post } from "@/models/post.model";
 import { NextRequest, NextResponse } from "next/server";
 
+// fetch all comments
+export const GET = async (req:NextRequest, {params}:{params:{postId:string}}) => {
+   
+}
