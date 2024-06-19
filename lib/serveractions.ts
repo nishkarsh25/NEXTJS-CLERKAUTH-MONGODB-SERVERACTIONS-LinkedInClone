bef9c708744eee0d157d7479f54c68ba8132488c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 10:24:39 +0530

    Import Statements Imports necessary modules and models: Post and Comment models from the application's models. IUser interface from the user model. currentUser function from @clerk/nextjs/server to get the current authenticated user. cloudinary for handling image uploads. connectDB function to connect to the MongoDB database. revalidatePath from next/cache to revalidate the Next.js cache after data changes. Cloudinary Configuration Configures Cloudinary with environment variables: cloud_name, api_key, and api_secret are set from environment variables to authenticate Cloudinary.

---
+++ lib/serveractions.ts
@@ -0,0 +1,17 @@
+"use server"
+
+import { Post } from "@/models/post.model";
+import { IUser } from "@/models/user.model";
+import { currentUser } from "@clerk/nextjs/server"
+import { v2 as cloudinary } from 'cloudinary';
+import connectDB from "./db";
+import { revalidatePath } from "next/cache";
+import { Comment } from "@/models/comment.model";
+
+
+
+
+
+
+
+
