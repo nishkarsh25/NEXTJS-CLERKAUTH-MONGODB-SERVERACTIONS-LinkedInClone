c3646fae4cdf984966fdfa751b6ce0d25585999f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 12:24:41 +0530

    JSX Structure Mapping Over Comments: Uses .map() to iterate over each comment in post.comments. Comment Component: Renders the Comment component for each comment, passing it the comment object as a prop and ensuring each component has a unique key (comment._id). Key Prop in Mapping key={comment._id}: Each rendered Comment component is given a unique key based on its _id property. This helps React efficiently update the DOM when comments are added, removed, or reordered. Conditional Rendering post?.comments?.map(): Ensures that .map() is only called if post and post.comments are not null or undefined. This prevents errors if post or post.comments are not yet defined.

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -2,7 +2,16 @@ import { IPostDocument } from '@/models/post.model'
 import React from 'react'
 import Comment from './Comment' 
 const Comments = ({post}:{post:IPostDocument}) => {
-  
-  
+  return (
+    <div> 
+        {
+            post?.comments?.map((comment: any)=>{
+                return (
+                    <Comment key={comment._id} comment={comment}/>
+                )
+            })
+        } 
+    </div>
+  )
 }
 
