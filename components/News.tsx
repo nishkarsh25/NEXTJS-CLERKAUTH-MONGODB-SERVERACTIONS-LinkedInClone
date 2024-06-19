0d265ac2ce379a02f7cfcb11e43d924a61086047
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 10:58:24 +0530

    Data Array newsItems Array: newsItems: NAVITEMS[]: An array of objects where each object represents a news item. Each item includes: heading: The main headline or title of the news. subHeading: Additional details about the news, typically including the time and number of readers.

--- components/News.tsx
+++ components/News.tsx
@@ -5,7 +5,24 @@ interface NAVITEMS {
   heading: string,
   subHeading: string
 }
-
+const newsItems: NAVITEMS[] = [
+  {
+    heading: "E-retailer retag health drinks",
+    subHeading: "4h ago - 345 readers"
+  },
+  {
+    heading: "Lets transport raises $22 million",
+    subHeading: "4h ago - 323 readers"
+  },
+  {
+    heading: "Casual waer is in at India Inc",
+    subHeading: "4h ago - 234 readers"
+  },
+  {
+    heading: "Snaller cities go on loans",
+    subHeading: "4h ago - 112 readers"
+  },
+]
 
 
 
