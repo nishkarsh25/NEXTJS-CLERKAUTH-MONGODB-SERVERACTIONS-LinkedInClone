b4850e28fad3ce32224a6e628de363287d1ac23d
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 10:52:28 +0530

    GET Function Defines the GET function to handle GET requests: This function retrieves the likes for a specific post. Parameters Function parameters: req: NextRequest: The request object, which contains information about the incoming HTTP request. {params}: {params: {postId: string}}: An object destructuring syntax to extract the postId parameter from the request. The postId is a string that represents the ID of the post for which likes are being fetched.

--- app/api/posts/[postId]/like/route.ts
+++ app/api/posts/[postId]/like/route.ts
@@ -2,4 +2,7 @@ import connectDB from "@/lib/db";
 import { Post } from "@/models/post.model";
 import { NextRequest, NextResponse } from "next/server";
 
-
+// get likes
+export const GET = async (req:NextRequest, {params}:{params:{postId:string}}) => {
+    
+}
