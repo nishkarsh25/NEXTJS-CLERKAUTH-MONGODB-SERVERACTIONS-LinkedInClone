be7dcdee7bc9f72532d0ed59034bf32915d13bd2
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 10:24:54 +0530

    Cloudinary Configuration Configures Cloudinary with environment variables: cloud_name, api_key, and api_secret are set from environment variables to authenticate Cloudinary.

--- lib/serveractions.ts
+++ lib/serveractions.ts
@@ -9,6 +9,11 @@ import { revalidatePath } from "next/cache";
 import { Comment } from "@/models/comment.model";
 
 
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
 
 
 
