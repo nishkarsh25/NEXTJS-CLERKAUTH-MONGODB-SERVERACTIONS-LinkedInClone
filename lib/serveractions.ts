9077d88bf9712ac3f372b5fce0a51d9828c38248
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-19 10:25:05 +0530

    createPostAction Function Defines createPostAction for creating posts: Connects to the database using connectDB. Retrieves the current user using currentUser. Validates if the user is authenticated and the input text is provided. Creates a userDatabase object with user details. If an image is provided, it uploads the image to Cloudinary and includes the image URL in the post. If no image is provided, it creates a text-only post. Revalidates the cache for the homepage ("/") to reflect the new post.

--- lib/serveractions.ts
+++ lib/serveractions.ts
@@ -8,14 +8,50 @@ import connectDB from "./db";
 import { revalidatePath } from "next/cache";
 import { Comment } from "@/models/comment.model";
 
-
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
     api_secret: process.env.API_SECRET
 });
 
+// creating post using server actions
+export const createPostAction = async (inputText: string, selectedFile: string) => {
+    await connectDB();
+    const user = await currentUser();
+    if (!user) throw new Error('User not athenticated');
+    if (!inputText) throw new Error('Input field is required');
 
+    const image = selectedFile;
+
+
+    const userDatabase: IUser = {
+        firstName: user.firstName || "Patel",
+        lastName: user.lastName || "Mern Stack",
+        userId: user.id,
+        profilePhoto: user.imageUrl
+    }
+    let uploadResponse;
+    try {
+        if (image) {
+            //1. create post with image
+            uploadResponse = await cloudinary.uploader.upload(image);
+            await Post.create({
+                description: inputText,
+                user: userDatabase,
+                imageUrl: uploadResponse?.secure_url // yha pr image url ayega from cloudinary
+            })
+        } else {
+            //2. create post with text only
+            await Post.create({
+                description: inputText,
+                user: userDatabase
+            })
+        }
+        revalidatePath("/");
+    } catch (error: any) {
+        throw new Error(error);
+    }
+}
 
 
 
